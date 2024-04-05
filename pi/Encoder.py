70ec8cb980814536d0585c2a8e575fd18333e990
Marshall Bluhm <bluhm101@d.umn.edu>
2024-04-05 10:36:38 -0500

    Added multithreading to the Encoder to get a close approximation of RPM of the shaft

--- pi/Encoder.py
+++ pi/Encoder.py
@@ -1,24 +1,56 @@
-#!/home/hydro/Documents/Senior-Design/DAQ/bin/python3
 from gpiozero import Button
-from signal import pause
+import threading 
+import time
 
-# Define the GPIO pin you want to monitor
-gpio_pin = 17
-
-# Create a Button object for the GPIO pin
-button = Button(gpio_pin, pull_up=False)  # If pull_up=True, use pull-up resistor
-
-# Initialize a counter variable
+# Global variable to be incremented
 press_count = 0
+prev_count = 0
+start_time = time.time()
+next_time = 0
+rpm = 0
+TPS_to_RPM_conversion = 60/2048
 
-# Define a callback function to be called when a rising edge is detected
-def on_rising_edge():
+gpio_pin = 17
+# button = Button(gpio_pin, pull_up=False)  # If pull_up=True, use pull-up resistor
+
+def counter():
     global press_count
-    press_count += 1
-    print("Rising edge detected! Press count:", press_count)
+    press_count += (2048)
+    #print("Rising edge detected! Press count:", press_count)
 
-# Assign the callback function to the Button's when_pressed event
-button.when_pressed = on_rising_edge
 
-# Keep the program running to allow event detection
-pause()
+def get_rpm():
+    while True: 
+        global press_count
+        global prev_count
+        global start_time
+        global rpm
+        current_time = time.time()
+        # print("The Start Time is " + str(start_time))
+        # print("The current time is " + str(current_time))
+        time_diff = (current_time - start_time)# in seconds
+        # print(time_diff)
+        start_time = current_time
+        count_diff = press_count - prev_count
+        prev_count = press_count #in ticks need to convert to rotations 
+        # print(count_diff)
+        if time_diff == 0:
+            rpm = 0
+        else:
+            rpm = (count_diff/time_diff) * TPS_to_RPM_conversion
+        print("rpm " + str(rpm))
+        i +=1
+        time.sleep(1)
+
+def Encoder():
+    while True:
+        button.when_pressed = counter
+
+
+# Create threads
+thread1 = threading.Thread(target=get_rpm)
+thread2 = threading.Thread(target=Encoder)
+
+# Start threads
+thread1.start()
+thread2.start()
